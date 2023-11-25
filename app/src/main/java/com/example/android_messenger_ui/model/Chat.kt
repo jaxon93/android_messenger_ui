bba86f83f9cb3d9dbc060b43bf14ef652d6686d7
jaxon93 <jaxon932002@gmail.com>
2023-11-26 07:20:37 +0900

    final

--- app/src/main/java/com/example/android_messenger_ui/model/Chat.kt
+++ app/src/main/java/com/example/android_messenger_ui/model/Chat.kt
@@ -2,7 +2,6 @@ package com.example.android_messenger_ui.model
 
 class Chat {
     var message: Message? = null
-<<<<<<< HEAD
     var rooms : ArrayList<Room> = ArrayList()
 
     constructor(message: Message) {
@@ -10,15 +9,6 @@ class Chat {
     }
 
     constructor(rooms: ArrayList<Room>) {
-=======
-    var rooms: ArrayList<Room> = ArrayList()
-
-    constructor(message: Message){
-        this.message = message
-    }
-
-    constructor(rooms: ArrayList<Room>){
->>>>>>> 9030880 (final)
-        this.rooms = rooms
+        var rooms: ArrayList<Room> = ArrayList()
     }
 }
